dad92bf3d723b448dc973a67e02322669f40d343
Maria Clara Bertin Vieira Ferreira <bertinmaria@dinfor.uniceub>
2026-08-20 21:39:55 -0300

    client projeto aula04

--- 04/react/projeto/server/index.js
+++ 04/react/projeto/server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Bem vindo!!!")
+    res.send("Bem vindo!!! Servidor está ok.")
 })
 
 app.listen(3001, () => {
